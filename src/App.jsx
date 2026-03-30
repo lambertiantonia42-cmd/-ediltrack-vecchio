b11322317c56a0e73d40530aac399ca09a1ebc4f
antolamb <lambertiantonia42@gmail.com>
2026-03-30 21:16:45 +0200

    fix vercel root corretto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
-import Sidebar from "../components/Sidebar";
-import Header from "../components/Header";
+
 
 export default function DashboardLayout({ children }) {
 
